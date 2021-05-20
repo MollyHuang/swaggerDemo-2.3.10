95a488428347d07c3078a989468dad17a7a67362
Molly Huang <molly.h@gmail.com>
2021-05-21 03:38:52 +0800

    #9 - Add annotation to Controller @Api(tags = "Hello控制類") @ApiOperation("hello控制類")

--- src/main/java/com/delta/swaggerDemo/controllers/HelloController.java
+++ src/main/java/com/delta/swaggerDemo/controllers/HelloController.java
@@ -1,10 +1,13 @@
 package com.delta.swaggerDemo.controllers;
 
 import com.delta.swaggerDemo.models.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "Hello控制類")
 public class HelloController {
     @GetMapping("hello")
     public String hello() {
@@ -12,6 +15,7 @@ public class HelloController {
     }
 
     @GetMapping("/getUser")
+    @ApiOperation("hello控制類")
     public User getUser() {
         return new User();
     }
