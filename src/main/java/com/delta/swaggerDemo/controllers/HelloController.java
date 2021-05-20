89648292bb575e318ecc5c4ae83777dce313d436
Molly Huang <molly.h@gmail.com>
2021-05-21 03:56:46 +0800

    @ApiParam - Controller method parameter

--- src/main/java/com/delta/swaggerDemo/controllers/HelloController.java
+++ src/main/java/com/delta/swaggerDemo/controllers/HelloController.java
@@ -3,7 +3,9 @@ package com.delta.swaggerDemo.controllers;
 import com.delta.swaggerDemo.models.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +20,10 @@ public class HelloController {
     @ApiOperation("hello控制類")
     public User getUser() {
         return new User();
+    }
+
+    @GetMapping("/username")
+    public String getUserName(@ApiParam("使用者名稱") String username) {
+        return username;
     }
 }
