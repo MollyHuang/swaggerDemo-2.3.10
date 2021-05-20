3bf467ed0128d1d519d9c8aa35404748b20b0321
Molly Huang <molly.h@gmail.com>
2021-05-21 03:35:24 +0800

    #8 - Add annotation to Entity Class @ApiModel @ApiModelProperty

--- src/main/java/com/delta/swaggerDemo/models/User.java
+++ src/main/java/com/delta/swaggerDemo/models/User.java
@@ -1,6 +1,12 @@
 package com.delta.swaggerDemo.models;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("使用者實體")
 public class User {
+    @ApiModelProperty("使用者名稱")
     public String username;
+    @ApiModelProperty("密碼")
     public String password;
 }
